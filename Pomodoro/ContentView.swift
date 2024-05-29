e4f3894bfedf45aa2626b2b157c04833b8dbddf7
manya-gup <gupta969@gmail.com>
2024-05-28 21:24:02 -0700

    Initial Commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Manya Gupta on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Manya Gupta on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
